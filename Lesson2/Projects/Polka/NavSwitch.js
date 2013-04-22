84bdea6f59539ea7483e9266ac8d8a4c76e013bc
Joost de Nijs <joostden@microsoft.com>
2013-04-22 00:03:34 -0700

    pushing lesson 2 progress

---
+++ Lesson2/Projects/Polka/NavSwitch.js
@@ -0,0 +1,36 @@
+﻿function clearAll() {
+    x = document.getElementById("leftnav").getElementsByTagName("a");
+    for (i = 0; i < x.length; i++) {
+        try{
+            x.item(i).setAttribute('class', x.item(i).getAttribute('class').replace('currentcontent', '').trim());
+        }
+        catch (e) {
+        }
+    }
+    x = document.getElementById("contentbody").children;
+    for (i = 0; i < x.length; i++) {
+        try{
+            x.item(i).setAttribute('class', x.item(i).getAttribute('class').replace('currentcontent', '').trim());
+        }
+        catch (e) {
+        }
+    }
+}
+
+function switchTo(element, body) {
+    clearAll();
+    try{
+        element.setAttribute('class', x.item(i).getAttribute('class') + ' currentcontent');
+    }
+    catch (e) {
+        element.setAttribute('class', 'currentcontent');
+    }
+    bodyelement = document.getElementById(body);
+    try {
+        y = bodyelement.getAttribute('class');
+        bodyelement.setAttribute('class', y + ' currentcontent');
+    }
+    catch (e) {
+        bodyelement.setAttribute('class', 'currentcontent');
+    }
+}
--- Lesson2/README.md
+++ Lesson2/README.md
@@ -4,6 +4,6 @@ Here are rules:
 
 1.       Show a list of tour dates (location, venue, date) (you can make them up)
 2.       The site must feature a fixed sidebar menu (doesn't scroll with page)
-3.       The site must feature a fixed full with header (doesn't scroll with page)
+3.       The site must feature a fixed full width header (doesn't scroll with page)
 4.       Add HTML5 audio and/or video
 5.       Has to work *exactly* the same in both IE and Chrome
