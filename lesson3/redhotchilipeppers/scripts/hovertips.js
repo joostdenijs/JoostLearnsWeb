2fd45690a30b77f5b478da43adf5877af7decedf
Joost de Nijs <joostden@microsoft.com>
2013-04-24 14:23:08 -0700

    more refactoring

---
+++ lesson3/redhotchilipeppers/scripts/hovertips.js
@@ -0,0 +1,33 @@
+﻿window.onload = init;
+function init() {
+    if (window.Event) {
+        document.captureEvents(Event.MOUSEMOVE);
+    }
+    document.onmousemove = getCursorXY;
+}
+
+function getCursorXY(e) {
+    sessionStorage.x = event.clientX;
+    sessionStorage.y = event.clientY;
+    activetips = document.getElementsByClassName('visiblehover');
+    for (i = 0; i < activetips.length; i++) {
+        positionAtCursor(activetips[i]);
+    }
+}
+
+function positionAtCursor(element) {
+
+    element.setAttribute('style', 'left: ' + sessionStorage.x + 'px; top: ' + sessionStorage.y + 'px;');
+}
+
+function on(element) {
+    removeClass(element, 'invisiblehover');
+    appendClass(element, 'visiblehover');
+    positionAtCursor(element);
+}
+
+function off(element) {
+    //TODO: add jQuery functionality so that we don't disable if the mouse is over the hovertip"
+    removeClass(element, 'visiblehover');
+    appendClass(element, 'invisiblehover');
+}
---
+++ lesson3/redhotchilipeppers/scripts/stylemanipulation.js
@@ -0,0 +1,16 @@
+﻿function appendClass(element, newclass) {
+    try {
+        element.setAttribute('class', element.getAttribute('class') + ' ' + newclass);
+    }
+    catch (e) {
+        element.setAttribute('class', newclass);
+    }
+}
+
+function removeClass(element, removedclass) {
+    try {
+        element.setAttribute('class', element.getAttribute('class').replace(removedclass, '').trim());
+    }
+    catch (e) {
+    }
+}
