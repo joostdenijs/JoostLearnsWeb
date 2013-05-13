2bbd1ff5be562b1f3396d9bb42d741825658b26c
Joost de Nijs <joostden@microsoft.com>
2013-05-13 01:54:20 -0700

    basic layout and navigation complete

---
+++ lesson4/resume/scripts/nav.js
@@ -0,0 +1,14 @@
+﻿function switchTo(element) {
+    if ($(element).hasClass('active')){
+        return true;
+    }
+    var currentSelections = $('.active');
+    for (i = 0; i < currentSelections.length; i++) {
+        $(currentSelections[i]).removeClass('active');
+    }
+
+    var newActive = $('.'+element.className);
+    for (i = 0; i < newActive.length; i++) {
+        $(newActive[i]).addClass('active');
+    }
+}
