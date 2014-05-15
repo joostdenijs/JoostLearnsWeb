7d895f7cbf23881de8f41f7f4219db0bc685cf10
Joost de Nijs <joostden@microsoft.com>
2014-05-15 15:42:00 -0700

    pushing the angular prototype up

---
+++ WIN Registration Prototype/app/controllers/TeamsController.js
@@ -0,0 +1,6 @@
+﻿app.controller('TeamsController', function ($scope) {
+    $scope.teams = [
+        { TeamName: "Team 1", MembershipNo: "147-164530", TeamId: 1 },
+        { TeamName: "Team 2", MembershipNo: "147-164531" }
+    ]
+});
---
+++ WIN Registration Prototype/app/registration.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="../Scripts/angular.min.js">
+var app = angular.module("WINRegistration", []);
+
+app.config((function ($routeProvider) {
+    $routeProvider
+        .when('/teams',
+            {
+                controller: 'TeamsController',
+                templateUrl: '/app/partials/teams.html'
+            })
+        //Define a route that has a route parameter in it (:customerID)
+        .when('/team/:teamID',
+            {
+                controller: 'TeamRegistrationController',
+                templateUrl: '/app/partials/customerOrders.html'
+            })
+        .otherwise({ redirectTo: '/teams' });
+});
